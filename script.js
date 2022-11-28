fdc719f15eaff915b464f3273f86a4246ab67f0c
FoxWithMatches <freiheiit92@mail.ru>
2022-11-28 12:38:16 +0300

    add notes without save localStorage

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function onPageLoaded() {
-    const input = document.querySelector(".notes-input");
-    const ul = document.querySelector(".ul.all-lists");
-
-    
+document.querySelector(".btn-notes").onclick = function(){
+    const newToDo = document.querySelector(".notes-input");
+    if(newToDo.value.length === 0){
+        alert("Введите заметку")
+    } else {
+        document.querySelector("ul.all-lists").innerHTML += 
+        `<ul class="all-lists">
+                <li class="notes-item">
+                    ${newToDo.value}
+                </li>
+            </ul>`;
+            newToDo.value = "";
+    }
 }
+
